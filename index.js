2c56652017cbab4634605922e50af1e70d08cfcf
JM12GH <jm1204.it@gmail.com>
2022-12-05 18:44:38 +0100

    Add another object for car and some properties related to it

---
+++ index.js
@@ -0,0 +1,11 @@
+const car={
+    color:"blue",
+    yearOfCreation: "2010",
+    mark:"Ford",
+}
+
+let cMark = car.mark;
+let cYear= car.yearOfCreation;
+let cColor= car.color;
+
+console.log(`My car is of type ${cMark},its creation year ${cYear},and its color ${cColor}`);
