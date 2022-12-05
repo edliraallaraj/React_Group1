6d4a5d6ed50e3dde13a14a44d1236cd309776047
arjana28 <arianaalla@outlook.com>
2022-12-05 18:44:24 +0100

    Add another object for car , its mark and its year

---
+++ index.js
@@ -0,0 +1,37 @@
+// Create an object called car   , give it some properties like color, year of creation and mark of it
+// Output on the console something like : "My car is of type , its creation year is and its color is"
+
+let cars = [
+{
+color: "blue",
+year: "2015",  
+mark: "Audi",
+},
+{
+
+color: "black",
+year: "2012",  
+mark: "BMW",
+},
+{
+color: "white",
+year: "2009",  
+mark: "Mercedes",
+},
+{
+color: "grey",
+year: "2018",  
+mark: "Range Rover",
+},       
+{
+color: "red",
+year: "2011",  
+mark: "Wolswagen",
+},                
+];
+    
+    
+    
+console.log(`My car is ${cars [2].mark} its creation year ${cars[1].year} and its color is  ${cars [1].color}`);
+
+   
